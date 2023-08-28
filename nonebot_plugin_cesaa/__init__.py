e8a237ae9c1f88997b1ec8c83530d43cd2a97564
uy_sun <hmy0119@gmail.com>
2023-08-28 12:31:05 +0800

    feat!: 与 nonebot_plugin_session 的函数命名保持一致

--- nonebot_plugin_cesaa/__init__.py
+++ nonebot_plugin_cesaa/__init__.py
@@ -3,8 +3,6 @@ from nonebot import require
 require("nonebot_plugin_chatrecorder")
 require("nonebot_plugin_saa")
 
-from .record import get_message_records_by_target as get_message_records_by_target
-from .record import get_messages_by_target as get_messages_by_target
-from .record import (
-    get_messages_plain_text_by_target as get_messages_plain_text_by_target,
-)
+from .record import get_message_records as get_message_records
+from .record import get_messages as get_messages
+from .record import get_messages_plain_text as get_messages_plain_text
