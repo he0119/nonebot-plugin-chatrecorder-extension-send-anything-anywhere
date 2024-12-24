87414a095fabbf83997fb40370696e6363f4f8a6
uy/sun <hmy0119@gmail.com>
2024-12-24 18:49:25 +0800

    feat: 适配新版 chatrecorder

--- nonebot_plugin_cesaa/record.py
+++ nonebot_plugin_cesaa/record.py
@@ -5,6 +5,7 @@ from typing import Optional
 from nonebot.adapters import Message
 from nonebot_plugin_chatrecorder import MessageRecord, deserialize_message
 from nonebot_plugin_chatrecorder.record import filter_statement
+from nonebot_plugin_chatrecorder.utils import scope_value
 from nonebot_plugin_orm import get_session
 from nonebot_plugin_saa import (
     PlatformTarget,
@@ -22,77 +23,84 @@ from nonebot_plugin_saa import (
     TargetTelegramCommon,
     TargetTelegramForum,
 )
-from nonebot_plugin_session_orm import SessionModel
+from nonebot_plugin_uninfo import SceneType, SupportScope
+from nonebot_plugin_uninfo.orm import BotModel, SceneModel, SessionModel, UserModel
 from sqlalchemy import ColumnElement, select
 
 
 def target_to_filter_statement(target: PlatformTarget) -> list[ColumnElement[bool]]:
     """将 PlatformTarget 转换为 chatrecorder 所需参数"""
-    platform = None
-    id1 = None
-    id2 = None
-    id3 = None
+    scope = None
+    scene_id = None
+    scene_type = None
     if isinstance(target, TargetQQPrivate):
-        platform = "qq"
-        id1 = str(target.user_id)
+        scope = SupportScope.qq_client
+        scene_id = str(target.user_id)
+        scene_type = SceneType.PRIVATE
     elif isinstance(target, TargetQQGroup):
-        platform = "qq"
-        id2 = str(target.group_id)
+        scope = SupportScope.qq_client
+        scene_id = str(target.group_id)
+        scene_type = SceneType.GROUP
     elif isinstance(target, TargetQQGuildDirect):
-        platform = "qqguild"
-        id1 = str(target.recipient_id)
-        id3 = str(target.source_guild_id)
+        scope = SupportScope.qq_guild
+        scene_id = str(target.recipient_id)
+        scene_type = SceneType.PRIVATE
     elif isinstance(target, TargetQQGuildChannel):
-        platform = "qqguild"
-        id2 = str(target.channel_id)
+        scope = SupportScope.qq_guild
+        scene_id = str(target.channel_id)
+        scene_type = SceneType.CHANNEL_TEXT
     elif isinstance(target, TargetKaiheilaPrivate):
-        platform = "kaiheila"
-        id1 = target.user_id
+        scope = SupportScope.kook
+        scene_id = target.user_id
+        scene_type = SceneType.PRIVATE
     elif isinstance(target, TargetKaiheilaChannel):
-        platform = "kaiheila"
-        id2 = target.channel_id
+        scope = SupportScope.kook
+        scene_id = target.channel_id
+        scene_type = SceneType.CHANNEL_TEXT
     elif isinstance(target, TargetFeishuPrivate):
-        platform = "feishu"
-        id1 = target.open_id
+        scope = SupportScope.feishu
+        scene_id = target.open_id
+        scene_type = SceneType.PRIVATE
     elif isinstance(target, TargetFeishuGroup):
-        platform = "feishu"
-        id2 = target.chat_id
+        scope = SupportScope.feishu
+        scene_id = target.chat_id
+        scene_type = SceneType.GROUP
     elif isinstance(target, TargetTelegramCommon):
-        platform = "telegram"
-        id1 = str(target.chat_id)
+        scope = SupportScope.telegram
+        scene_id = str(target.chat_id)
+        scene_type = SceneType.PRIVATE
     elif isinstance(target, TargetTelegramForum):
-        platform = "telegram"
-        id2 = str(target.chat_id)
-        id3 = str(target.message_thread_id)
+        scope = SupportScope.telegram
+        scene_id = str(target.message_thread_id)
+        scene_type = SceneType.CHANNEL_CATEGORY
     elif isinstance(target, TargetDoDoPrivate):
-        platform = "dodo"
-        id1 = target.dodo_source_id
+        scope = SupportScope.dodo
+        scene_id = target.dodo_source_id
     elif isinstance(target, TargetDoDoChannel):
-        platform = "dodo"
-        id2 = target.channel_id
+        scope = SupportScope.dodo
+        scene_id = target.channel_id
+        scene_type = SceneType.CHANNEL_TEXT
     elif isinstance(target, TargetOB12Unknow):
+        scope = SupportScope.ensure_ob12(target.platform)
         if target.detail_type == "private":
-            platform = target.platform
-            id1 = target.user_id
+            scene_id = target.user_id
+            scene_type = SceneType.PRIVATE
         elif target.detail_type == "group":
-            platform = target.platform
-            id2 = target.group_id
+            scene_id = target.group_id
+            scene_type = SceneType.GROUP
         else:
-            platform = target.platform
-            id2 = target.channel_id
-            id3 = target.guild_id
+            scene_id = target.channel_id
+            scene_type = SceneType.CHANNEL_TEXT
     else:
         raise ValueError(f"不支持的 PlatformTarget 类型：{target}")
 
     whereclause: list[ColumnElement[bool]] = []
-    if platform is not None:
-        whereclause.append(SessionModel.platform == platform)
-    if id1 is not None:
-        whereclause.append(SessionModel.id1 == id1)
-    if id2 is not None:
-        whereclause.append(SessionModel.id2 == id2)
-    if id3 is not None:
-        whereclause.append(SessionModel.id3 == id3)
+    if scope is not None:
+        whereclause.append(BotModel.scope == scope_value(scope))
+    if scene_id:
+        whereclause.append(SceneModel.scene_id == scene_id)
+    if scene_type is not None:
+        whereclause.append(SceneModel.scene_type == scene_type.value)
     return whereclause
 
 
@@ -105,19 +113,23 @@ async def get_message_records(
       * ``target: Optional[PlatformTarget]``: 发送目标，传入时会根据 `PlatformTarget` 中的字段筛选
       * ``session: Optional[Session]``: 会话模型，传入时会根据 `session` 中的字段筛选
       * ``id_type: SessionIdType``: 会话 id 类型，仅在传入 `session` 时有效
-      * ``include_platform: bool``: 是否限制平台类型，仅在传入 `session` 时有效
-      * ``include_bot_type: bool``: 是否限制适配器类型，仅在传入 `session` 时有效
-      * ``include_bot_id: bool``: 是否限制 bot id，仅在传入 `session` 时有效
-      * ``bot_ids: Optional[Iterable[str]]``: bot id 列表，为空表示所有 bot id
-      * ``bot_types: Optional[Iterable[str]]``: 协议适配器类型列表，为空表示所有适配器
-      * ``platforms: Optional[Iterable[str]]``: 平台类型列表，为空表示所有平台
-      * ``levels: Optional[Iterable[Union[str, SessionLevel]]]``: 会话级别列表，为空表示所有级别
-      * ``id1s: Optional[Iterable[str]]``: 会话 id1（用户级 id）列表，为空表示所有 id
-      * ``id2s: Optional[Iterable[str]]``: 会话 id2（群组级 id）列表，为空表示所有 id
-      * ``id3s: Optional[Iterable[str]]``: 会话 id3（两级群组级 id）列表，为空表示所有 id
-      * ``exclude_id1s: Optional[Iterable[str]]``: 不包含的会话 id1（用户级 id）列表，为空表示不限制
-      * ``exclude_id2s: Optional[Iterable[str]]``: 不包含的会话 id2（群组级 id）列表，为空表示不限制
-      * ``exclude_id3s: Optional[Iterable[str]]``: 不包含的会话 id3（两级群组级 id）列表，为空表示不限制
+      * ``filter_self_id: bool``: 是否筛选 bot id，仅在传入 `session` 时有效
+      * ``filter_adapter: bool``: 是否筛选适配器类型，仅在传入 `session` 时有效
+      * ``filter_scope: bool``: 是否筛选平台类型，仅在传入 `session` 时有效
+      * ``filter_scene: bool``: 是否筛选事件场景，仅在传入 `session` 时有效
+      * ``filter_user: bool``: 是否筛选用户，仅在传入 `session` 时有效
+      * ``self_ids: Optional[Iterable[str]]``: bot id 列表，为空表示所有 bot id
+      * ``adapters: Optional[Iterable[Union[str, SupportAdapter]]]``: 适配器类型列表，为空表示所有适配器
+      * ``scopes: Optional[Iterable[Union[str, SupportScope]]]``: 平台类型列表，为空表示所有平台
+      * ``scene_types: Optional[Iterable[Union[str, SceneType]]]``: 事件场景类型列表，为空表示所有类型
+      * ``scene_ids: Optional[Iterable[str]]``: 事件场景 id 列表，为空表示所有 id
+      * ``user_ids: Optional[Iterable[str]]``: 用户 id 列表，为空表示所有 id
+      * ``exclude_self_ids: Optional[Iterable[str]]``: 不包含的 bot id 列表，为空表示不限制
+      * ``exclude_adapters: Optional[Iterable[Union[str, SupportAdapter]]]``: 不包含的适配器类型列表，为空表示不限制
+      * ``exclude_scopes: Optional[Iterable[Union[str, SupportScope]]]``: 不包含的平台类型列表，为空表示不限制
+      * ``exclude_scene_types: Optional[Iterable[Union[str, SceneType]]]``: 不包含的事件场景类型列表，为空表示不限制
+      * ``exclude_scene_ids: Optional[Iterable[str]]``: 不包含的事件场景 id 列表，为空表示不限制
+      * ``exclude_user_ids: Optional[Iterable[str]]``: 不包含的用户 id 列表，为空表示不限制
       * ``time_start: Optional[datetime]``: 起始时间，为空表示不限制起始时间（传入带时区的时间或 UTC 时间）
       * ``time_stop: Optional[datetime]``: 结束时间，为空表示不限制结束时间（传入带时区的时间或 UTC 时间）
       * ``types: Optional[Iterable[Literal["message", "message_sent"]]]``: 消息事件类型列表，为空表示所有类型
@@ -132,6 +144,9 @@ async def get_message_records(
         select(MessageRecord)
         .where(*whereclause)
         .join(SessionModel, SessionModel.id == MessageRecord.session_persist_id)
+        .join(BotModel, BotModel.id == SessionModel.bot_persist_id)
+        .join(SceneModel, SceneModel.id == SessionModel.scene_persist_id)
+        .join(UserModel, UserModel.id == SessionModel.user_persist_id)
     )
     async with get_session() as db_session:
         records = (await db_session.scalars(statement)).all()
@@ -147,19 +162,23 @@ async def get_messages(
       * ``target: Optional[PlatformTarget]``: 发送目标，传入时会根据 `PlatformTarget` 中的字段筛选
       * ``session: Optional[Session]``: 会话模型，传入时会根据 `session` 中的字段筛选
       * ``id_type: SessionIdType``: 会话 id 类型，仅在传入 `session` 时有效
-      * ``include_platform: bool``: 是否限制平台类型，仅在传入 `session` 时有效
-      * ``include_bot_type: bool``: 是否限制适配器类型，仅在传入 `session` 时有效
-      * ``include_bot_id: bool``: 是否限制 bot id，仅在传入 `session` 时有效
-      * ``bot_ids: Optional[Iterable[str]]``: bot id 列表，为空表示所有 bot id
-      * ``bot_types: Optional[Iterable[str]]``: 协议适配器类型列表，为空表示所有适配器
-      * ``platforms: Optional[Iterable[str]]``: 平台类型列表，为空表示所有平台
-      * ``levels: Optional[Iterable[Union[str, SessionLevel]]]``: 会话级别列表，为空表示所有级别
-      * ``id1s: Optional[Iterable[str]]``: 会话 id1（用户级 id）列表，为空表示所有 id
-      * ``id2s: Optional[Iterable[str]]``: 会话 id2（群组级 id）列表，为空表示所有 id
-      * ``id3s: Optional[Iterable[str]]``: 会话 id3（两级群组级 id）列表，为空表示所有 id
-      * ``exclude_id1s: Optional[Iterable[str]]``: 不包含的会话 id1（用户级 id）列表，为空表示不限制
-      * ``exclude_id2s: Optional[Iterable[str]]``: 不包含的会话 id2（群组级 id）列表，为空表示不限制
-      * ``exclude_id3s: Optional[Iterable[str]]``: 不包含的会话 id3（两级群组级 id）列表，为空表示不限制
+      * ``filter_self_id: bool``: 是否筛选 bot id，仅在传入 `session` 时有效
+      * ``filter_adapter: bool``: 是否筛选适配器类型，仅在传入 `session` 时有效
+      * ``filter_scope: bool``: 是否筛选平台类型，仅在传入 `session` 时有效
+      * ``filter_scene: bool``: 是否筛选事件场景，仅在传入 `session` 时有效
+      * ``filter_user: bool``: 是否筛选用户，仅在传入 `session` 时有效
+      * ``self_ids: Optional[Iterable[str]]``: bot id 列表，为空表示所有 bot id
+      * ``adapters: Optional[Iterable[Union[str, SupportAdapter]]]``: 适配器类型列表，为空表示所有适配器
+      * ``scopes: Optional[Iterable[Union[str, SupportScope]]]``: 平台类型列表，为空表示所有平台
+      * ``scene_types: Optional[Iterable[Union[str, SceneType]]]``: 事件场景类型列表，为空表示所有类型
+      * ``scene_ids: Optional[Iterable[str]]``: 事件场景 id 列表，为空表示所有 id
+      * ``user_ids: Optional[Iterable[str]]``: 用户 id 列表，为空表示所有 id
+      * ``exclude_self_ids: Optional[Iterable[str]]``: 不包含的 bot id 列表，为空表示不限制
+      * ``exclude_adapters: Optional[Iterable[Union[str, SupportAdapter]]]``: 不包含的适配器类型列表，为空表示不限制
+      * ``exclude_scopes: Optional[Iterable[Union[str, SupportScope]]]``: 不包含的平台类型列表，为空表示不限制
+      * ``exclude_scene_types: Optional[Iterable[Union[str, SceneType]]]``: 不包含的事件场景类型列表，为空表示不限制
+      * ``exclude_scene_ids: Optional[Iterable[str]]``: 不包含的事件场景 id 列表，为空表示不限制
+      * ``exclude_user_ids: Optional[Iterable[str]]``: 不包含的用户 id 列表，为空表示不限制
       * ``time_start: Optional[datetime]``: 起始时间，为空表示不限制起始时间（传入带时区的时间或 UTC 时间）
       * ``time_stop: Optional[datetime]``: 结束时间，为空表示不限制结束时间（传入带时区的时间或 UTC 时间）
       * ``types: Optional[Iterable[Literal["message", "message_sent"]]]``: 消息事件类型列表，为空表示所有类型
@@ -171,9 +190,12 @@ async def get_messages(
     if target:
         whereclause.extend(target_to_filter_statement(target))
     statement = (
-        select(MessageRecord.message, SessionModel.bot_type)
+        select(MessageRecord.message, BotModel.adapter)
         .where(*whereclause)
         .join(SessionModel, SessionModel.id == MessageRecord.session_persist_id)
+        .join(BotModel, BotModel.id == SessionModel.bot_persist_id)
+        .join(SceneModel, SceneModel.id == SessionModel.scene_persist_id)
+        .join(UserModel, UserModel.id == SessionModel.user_persist_id)
     )
     async with get_session() as db_session:
         results = (await db_session.execute(statement)).all()
@@ -189,19 +211,23 @@ async def get_messages_plain_text(
       * ``target: Optional[PlatformTarget]``: 发送目标，传入时会根据 `PlatformTarget` 中的字段筛选
       * ``session: Optional[Session]``: 会话模型，传入时会根据 `session` 中的字段筛选
       * ``id_type: SessionIdType``: 会话 id 类型，仅在传入 `session` 时有效
-      * ``include_platform: bool``: 是否限制平台类型，仅在传入 `session` 时有效
-      * ``include_bot_type: bool``: 是否限制适配器类型，仅在传入 `session` 时有效
-      * ``include_bot_id: bool``: 是否限制 bot id，仅在传入 `session` 时有效
-      * ``bot_ids: Optional[Iterable[str]]``: bot id 列表，为空表示所有 bot id
-      * ``bot_types: Optional[Iterable[str]]``: 协议适配器类型列表，为空表示所有适配器
-      * ``platforms: Optional[Iterable[str]]``: 平台类型列表，为空表示所有平台
-      * ``levels: Optional[Iterable[Union[str, SessionLevel]]]``: 会话级别列表，为空表示所有级别
-      * ``id1s: Optional[Iterable[str]]``: 会话 id1（用户级 id）列表，为空表示所有 id
-      * ``id2s: Optional[Iterable[str]]``: 会话 id2（群组级 id）列表，为空表示所有 id
-      * ``id3s: Optional[Iterable[str]]``: 会话 id3（两级群组级 id）列表，为空表示所有 id
-      * ``exclude_id1s: Optional[Iterable[str]]``: 不包含的会话 id1（用户级 id）列表，为空表示不限制
-      * ``exclude_id2s: Optional[Iterable[str]]``: 不包含的会话 id2（群组级 id）列表，为空表示不限制
-      * ``exclude_id3s: Optional[Iterable[str]]``: 不包含的会话 id3（两级群组级 id）列表，为空表示不限制
+      * ``filter_self_id: bool``: 是否筛选 bot id，仅在传入 `session` 时有效
+      * ``filter_adapter: bool``: 是否筛选适配器类型，仅在传入 `session` 时有效
+      * ``filter_scope: bool``: 是否筛选平台类型，仅在传入 `session` 时有效
+      * ``filter_scene: bool``: 是否筛选事件场景，仅在传入 `session` 时有效
+      * ``filter_user: bool``: 是否筛选用户，仅在传入 `session` 时有效
+      * ``self_ids: Optional[Iterable[str]]``: bot id 列表，为空表示所有 bot id
+      * ``adapters: Optional[Iterable[Union[str, SupportAdapter]]]``: 适配器类型列表，为空表示所有适配器
+      * ``scopes: Optional[Iterable[Union[str, SupportScope]]]``: 平台类型列表，为空表示所有平台
+      * ``scene_types: Optional[Iterable[Union[str, SceneType]]]``: 事件场景类型列表，为空表示所有类型
+      * ``scene_ids: Optional[Iterable[str]]``: 事件场景 id 列表，为空表示所有 id
+      * ``user_ids: Optional[Iterable[str]]``: 用户 id 列表，为空表示所有 id
+      * ``exclude_self_ids: Optional[Iterable[str]]``: 不包含的 bot id 列表，为空表示不限制
+      * ``exclude_adapters: Optional[Iterable[Union[str, SupportAdapter]]]``: 不包含的适配器类型列表，为空表示不限制
+      * ``exclude_scopes: Optional[Iterable[Union[str, SupportScope]]]``: 不包含的平台类型列表，为空表示不限制
+      * ``exclude_scene_types: Optional[Iterable[Union[str, SceneType]]]``: 不包含的事件场景类型列表，为空表示不限制
+      * ``exclude_scene_ids: Optional[Iterable[str]]``: 不包含的事件场景 id 列表，为空表示不限制
+      * ``exclude_user_ids: Optional[Iterable[str]]``: 不包含的用户 id 列表，为空表示不限制
       * ``time_start: Optional[datetime]``: 起始时间，为空表示不限制起始时间（传入带时区的时间或 UTC 时间）
       * ``time_stop: Optional[datetime]``: 结束时间，为空表示不限制结束时间（传入带时区的时间或 UTC 时间）
       * ``types: Optional[Iterable[Literal["message", "message_sent"]]]``: 消息事件类型列表，为空表示所有类型
@@ -216,6 +242,9 @@ async def get_messages_plain_text(
         select(MessageRecord.plain_text)
         .where(*whereclause)
         .join(SessionModel, SessionModel.id == MessageRecord.session_persist_id)
+        .join(BotModel, BotModel.id == SessionModel.bot_persist_id)
+        .join(SceneModel, SceneModel.id == SessionModel.scene_persist_id)
+        .join(UserModel, UserModel.id == SessionModel.user_persist_id)
     )
     async with get_session() as db_session:
         records = (await db_session.scalars(statement)).all()
