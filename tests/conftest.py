3b0d477c88000b34b22429651e6952f246ce0340
uy/sun <hmy0119@gmail.com>
2024-12-24 19:20:50 +0800

    test: 使用 nonebug 最新写法

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import nonebot
 import pytest
-from nonebug import NONEBOT_INIT_KWARGS, App
+from nonebug import NONEBOT_INIT_KWARGS, NONEBOT_START_LIFESPAN, App
 from pytest_mock import MockerFixture
 from sqlalchemy import StaticPool, delete
 
@@ -14,26 +14,28 @@ def pytest_configure(config: pytest.Config) -> None:
         "driver": "~none",
         "alembic_startup_check": False,
     }
+    config.stash[NONEBOT_START_LIFESPAN] = False
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def after_nonebot_init(after_nonebot_init: None):
+    # 加载插件
+    nonebot.load_plugin("nonebot_plugin_cesaa")
 
 
 @pytest.fixture
-async def app(tmp_path: Path, mocker: MockerFixture):
-    # 加载插件
-    nonebot.require("nonebot_plugin_cesaa")
-    from nonebot_plugin_chatrecorder.model import MessageRecord
+async def app(app: App, tmp_path: Path, mocker: MockerFixture):
+    from nonebot_plugin_chatrecorder import MessageRecord
     from nonebot_plugin_orm import get_session, init_orm
-    from nonebot_plugin_uninfo.orm import BotModel, SceneModel, SessionModel, UserModel
+    from nonebot_plugin_uninfo.orm import SessionModel
 
     mocker.patch("nonebot_plugin_orm._data_dir", tmp_path / "orm")
 
     await init_orm()
 
-    yield App()
+    yield app
 
     # 清理数据
     async with get_session() as session, session.begin():
         await session.execute(delete(MessageRecord))
         await session.execute(delete(SessionModel))
-        await session.execute(delete(SceneModel))
-        await session.execute(delete(BotModel))
-        await session.execute(delete(UserModel))
